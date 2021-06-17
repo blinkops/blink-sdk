fe2c56ab177b9d67b4b71635720db07b7759189d
Ofir Pernik <ofir@blinkops.com>
2021-06-17 11:46:30 +0300

    Add pattern and options to action parameter

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -9,10 +9,12 @@ import (
 )
 
 type ActionParameter struct {
-	Type        string `yaml:"type"`
-	Description string `yaml:"description"`
-	Required    bool   `yaml:"required"`
-	Default     string `yaml:"default"`
+	Type        string   `yaml:"type"`
+	Description string   `yaml:"description"`
+	Required    bool     `yaml:"required"`
+	Pattern     string   `json:"pattern"` // optional: regex to validate in case of input component
+	Options     []string `json:"options"` // optional: the option list in case of dropdown\checkbox
+	Default     string   `yaml:"default"`
 }
 
 type Action struct {
