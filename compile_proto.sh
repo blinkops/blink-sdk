598efd4d8e28dc13683bacdab9aa64fafff934fa
Yuval Rimar <yuval@blinkops.com>
2021-08-02 13:30:18 +0300

    after steampipe upgrade collision in plugin.proto name causes panic

--- compile_proto.sh
+++ compile_proto.sh
@@ -5,4 +5,4 @@
 # go get -u github.com/golang/protobuf/protoc-gen-go
 # go get -u google.golang.org/grpc/cmd/protoc-gen-go-grpc
 
-protoc --go_out=. --go_opt=paths=source_relative  --go-grpc_out=. --go-grpc_opt=paths=source_relative plugin/proto/plugin.proto
+protoc --go_out=. --go_opt=paths=source_relative  --go-grpc_out=. --go-grpc_opt=paths=source_relative plugin/proto/integration_pack.proto
